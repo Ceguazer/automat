3339e59a312c38ddc165da9929018e09a46d6124
Robcio <robert.koziolek@b4f-software.com>
2016-02-28 21:13:17 +0100

    usunalem komcia

--- ConsoleApplication1/Automatum.cs
+++ ConsoleApplication1/Automatum.cs
@@ -18,7 +18,6 @@ namespace ConsoleApplication1
         public bool checkString(string word)
         {
             reset();
-            //dupaaaaaaa to dupa
             char[] array = word.ToArray();
             bool result = false;
             foreach (char character in array)
